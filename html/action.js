9f30bdbe6e43a0ffd22abbc9a54e16b0f89be07a
RGGH <pisscompany@gmail.com>
2022-08-04 15:37:31 +0100

    working html and js

--- html/action.js
+++ html/action.js
@@ -1,30 +1,25 @@
 async function SubmitVars() {
 
-
+    // data sent from the POST request
     var formData = new FormData(document.forms[0])
+
     // get all form keys and values
     var obj = Object.fromEntries(Array.from(formData.keys())
-        .map(key => [key, formData.getAll(key).length > 1 ? 
-        formData.getAll(key) : formData.get(key)]))
+        .map(key => [key, formData.getAll(key).length > 1 ?
+            formData.getAll(key) : formData.get(key)]))
 
-    var jsonreq  = (`${JSON.stringify(obj)}`)
+    var jsonreq = (`${JSON.stringify(obj)}`)
 
-    const options = {
-        method: 'POST',
-        body:  jsonreq,
-        headers: {"Content-type": "application/json; charset=UTF-8"}
-    };
+    const response = await fetch('http://127.0.0.1:8000/v1/check-ip/', {
+    method: "POST",
+    body: jsonreq,
+    headers: {"Content-type": "application/json; charset=UTF-8"}
+  })
 
-    const result = await fetch('http://127.0.0.1:8000/v1/check-ip/', options )
-        .then( response => response.json())
-        .then( response => {
-        console.log (response);
-        let results = response;
-        document.write(results.result)
-    } );
-    
-    //document.write("demo").innerHTML = result;
-    
-  
-
+  const responseText = await response.text();
+  console.log(responseText); // logs 'OK'
+  var x = document.getElementById("answer");  
+  x.style.color = "red"; 
+  x.innerHTML = JSON.stringify(responseText);
+  x.title = "FOOBAR";
 }
